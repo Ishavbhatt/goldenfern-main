99774c892b5a31d438e466aa8ac6e4f835db466b
Ishav BHatt <ishavbhattkhaniyara@gmail.com>
2022-05-19 15:48:57 +0530

    env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+
 module.exports = {
   env: {
     API_KEY: process.env.API_KEY,
--- pages/index.js
+++ pages/index.js
@@ -109,6 +109,7 @@ export default function Home() {
   // Increment People
   const incPeople = () => {
     setPeople(people + 1);
+    console.log(weatherapiUrl)
   };
 
   // Decrement People
