18c72e46d48a6e92a69a5a9f6254ba260518fb02
Ishav BHatt <ishavbhattkhaniyara@gmail.com>
2022-05-31 15:09:33 +0530

    add some things

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Head from "next/head";
 import DatePicker from "react-datepicker";
 import axios from "axios";
 import Link from "next/link";
@@ -31,7 +32,6 @@ let curruntDate = new Date().toLocaleDateString("en-us", {
   year: "numeric",
 });
 
-import Head from "next/head";
 
 const Home = ({ token, ...props }) => {
   const [people, setPeople] = useState(1);
